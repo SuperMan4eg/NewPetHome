8f91144f84e2da4b2fca9c5e5b268d384224e57c
SuperMan4eg <79028163+SuperMan4eg@users.noreply.github.com>
2024-08-10 13:10:16 +0300

    changed types Weight, Height and Requisites, fixed name Spesies

--- NewPetHome.Backend/Src/NewPetHome.Domain/Pet.cs
+++ NewPetHome.Backend/Src/NewPetHome.Domain/Pet.cs
@@ -2,21 +2,23 @@
 
 public class Pet
 {
+    private readonly List<Requisites> _requisites;
+    
     public Guid Id { get;private set; }
     public string Name { get;private set; }
-    public string species { get;private set; }
+    public string Species { get;private set; }
     public string Description { get;private set; }
     public string Breed { get;private set; }
     public string Color { get;private set; }
     public string HealthInfo { get;private set; }
     public string Address { get;private set; }
-    public int Weight { get;private set; }
-    public int Height { get;private set; }
+    public double Weight { get;private set; }
+    public double Height { get;private set; }
     public string PhoneNumber { get;private set; }
     public bool IsCastrated { get;private set; }
     public DateOnly BirthDate { get;private set; }
     public bool IsVaccinated { get;private set; }
     public string Status { get;private set; }
-    public Requisites Requisites { get;private set; }
+    public IReadOnlyList<Requisites> Requisites => _requisites;
     public DateTime CreatedDate { get; private set; }
 }
