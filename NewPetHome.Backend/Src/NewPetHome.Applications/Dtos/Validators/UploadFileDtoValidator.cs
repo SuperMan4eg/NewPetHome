590dee9a265170c44dc08e29085e9120c8f8c9f9
Андрей Ежов <andrei__92@mail.ru>
2024-09-19 12:01:02 +0300

    Add tests for add pet handler and upload file to pet handler

--- NewPetHome.Backend/Src/NewPetHome.Applications/Dtos/Validators/UploadFileDtoValidator.cs
+++ NewPetHome.Backend/Src/NewPetHome.Applications/Dtos/Validators/UploadFileDtoValidator.cs
@@ -8,7 +8,10 @@ public class UploadFileDtoValidator : AbstractValidator<UploadFileDto>
 {
     public UploadFileDtoValidator()
     {
-        RuleFor(u => u.FileName).NotEmpty().WithError(Errors.General.ValueIsRequired());
+        RuleFor(u => u.FileName)
+            .NotEmpty()
+            .Must(f => f.Contains(".jpg") || f.Contains(".jpeg") || f.Contains(".png"))
+            .WithError(Errors.General.ValueIsRequired());
         RuleFor(u => u.Content).Must(c => c.Length < 5000000);
     }
 }
